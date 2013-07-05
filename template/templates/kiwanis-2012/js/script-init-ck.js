518cb82764438ca73135009b1f11425b4ff09879
Jonathan Bennett <jbennett@ccistudios.com>
2013-07-05 19:59:20 -0400

    compressed some JS

---
+++ template/templates/kiwanis-2012/js/script-init-ck.js
@@ -0,0 +1,1 @@
+window.addEvent("domready",function(){new CCI.Rollover("img.rollover");new CCI.Rollover("li.item-122 img, li.item-105 img",{normal_text:".",over_text:"_rollover."});$$("h1, h2, h3, h4, h5, h6").lettering("words")});window.addEvent("load",function(){c=new CCI.Columns($("bottom"),".module > div > div > div",{offset:6})});
